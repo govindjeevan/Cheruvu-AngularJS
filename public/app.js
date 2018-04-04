99d1c5fc4b07c1b8051dd47f5c0dd0efdfd00ddf
govindjeevan <govindjeevan7@gmail.com>
2018-04-04 14:58:29 +0530

    Submission Stat

--- public/app.js
+++ public/app.js
@@ -6,5 +6,7 @@ app.controller('mainController', function($scope, $http) {
   $scope.submitForm = function() {
     console.log($scope.farmer);
     $http.post('/data', $scope.farmer)
+    $scope.submitted=true;
+    
     }
 });
